e9d947734d9a77a59f65040f1d557ca383a048a2
Nate Hobi <nate.hobi@gmail.com>
2016-06-13 11:21:05 -0400

    Init to win it

---
+++ includes/class-wp-m3-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       natehobi.com
+ * @since      1.0.0
+ *
+ * @package    Wp_M3
+ * @subpackage Wp_M3/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_M3
+ * @subpackage Wp_M3/includes
+ * @author     Nate Hobi <nate.hobi@gmail.com>
+ */
+class Wp_M3_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-m3-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       natehobi.com
+ * @since      1.0.0
+ *
+ * @package    Wp_M3
+ * @subpackage Wp_M3/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_M3
+ * @subpackage Wp_M3/includes
+ * @author     Nate Hobi <nate.hobi@gmail.com>
+ */
+class Wp_M3_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
